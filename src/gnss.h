9934e0fa128fefbabc8a6da5354ba63b0f005958
StevenCellist <steven@boonstoppel.nu>
2026-05-24 13:42:39 +0200

    Remove unused variable

--- src/gnss.h
+++ src/gnss.h
@@ -18,7 +18,7 @@ enum GpsFixLevel {
 int gpsFixLevel = GPS_NO_FIX;
 int prevFixLevel = GPS_NO_FIX;
 
-char dateBuf[11], newDateBuf[11], timeBuf[9];
+char dateBuf[11], newDateBuf[11];
 int numConsecutiveFix = 0;
 
 void setSystemTimeFromGPS() {
